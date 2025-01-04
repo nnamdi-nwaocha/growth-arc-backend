7b33aaba1efa189a17548e44f6386ef95aba7402
nnamdi nwaocha <nnamdinwaocha@gmail.com>
2025-01-04 02:58:17 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your frontend domain
+    credentials: true, // Allow cookies to be sent
+  });
   await app.listen(3000);
 }
 bootstrap();
