2ec21559df28a551b4d443ddc66f8b4f51481ccd
Nnamdi Nwaocha <nnamdinwaocha@gmail.com>
2025-01-17 12:25:53 +0100

    after merge

--- src/users/models/user.entity.ts
+++ src/users/models/user.entity.ts
@@ -2,33 +2,33 @@ import { Entity, PrimaryGeneratedColumn, Column, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryColumn()
-    user_id: string;
+  @PrimaryColumn()
+  user_id: string;
 
-    @Column({ unique: true })
-    email: string;
+  @Column({ unique: true })
+  email: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 
-    @Column()
-    firstname: string;
+  @Column()
+  firstname: string;
 
-    @Column()
-    lastname: string;
+  @Column()
+  lastname: string;
 
-    @Column({ default: false })
-    is_email_confirmed: boolean;
+  @Column({ default: false })
+  is_email_confirmed: boolean;
 
-    @Column({ nullable: true })
-    refresh_token: string;
+  @Column({ nullable: true })
+  refresh_token: string;
 
-    @Column({ default: false })
-    is_teacher: boolean;
+  @Column({ default: false })
+  is_teacher: boolean;
 
-    @Column({ nullable: true })
-    teacher_bio?: string;
+  @Column({ nullable: true })
+  teacher_bio?: string;
 
-    @Column({ nullable: true })
-    profile_picture_url?: string;
+  @Column({ nullable: true })
+  profile_picture_url?: string;
 }
